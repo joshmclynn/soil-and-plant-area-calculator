fcf13b6678c3eb65e762558962f6697867c14ff0
joshmclynn <mclynn_josh@hotmail.co.uk>
2021-12-01 21:48:18 +0000

    initial

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+SOIL = [50,100,1000]
+SOILPRICE = [3.99,10,65]
+PLANTSIZE = [1,5,10]
+PLANTPRICE = [1.99,2.99,15.99]
+
+def calcsoil():
+    print("Please input the length of the area and press enter")
+    length = float(input())
+
+    print("Please enter the width of the area and press enter")
+    width = float(input())
+
+    print("Please enter the depth of the area and press enter")
+    depth = float(input())
+
+    volume = length * width * depth
+
+    return volume
+
+calcsoil()
+
+bulkbag(volume)   
+
+    
+def bulkbag(volume):
+        bag = SOILPRICE[2]
+        total = bag / volume
+
+        print("If you used bulk-bags you would need", total ,"bags")
+
+        
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
