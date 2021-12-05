80f3f2cdcd68aa432530248ab92fff558f7e6315
joshmclynn <mclynn_josh@hotmail.co.uk>
2021-12-05 16:53:43 +0000

    adding main function shows when user loads application

--- run.py
+++ run.py
@@ -145,6 +145,11 @@ def payment():
     
     print("Your order number is",orderno,"you can use it to cancel your order")
 
+    print("Press 1 to return to the main menu")
+    if input()== "1":
+        main()
+    else:
+        print("Press 1 to return to the main menu")
     row = [orderno,cardno,pricedue]
     ORDERS.append_row(row)
 
@@ -152,3 +157,38 @@ def payment():
 
 
 calcarea()
+
+def plantcalc():
+
+
+def cancelorder():
+    userorder = input()
+    print("Please enter your order number")
+    if userorder == 
+
+
+
+
+
+
+def main():
+    print("""
+    
+   _____           _   _                         _             _                   _                      _                  _           _                  
+  / ____|         (_) | |                       | |           | |                 | |                    | |                | |         | |                 
+ | (___     ___    _  | |     __ _   _ __     __| |    _ __   | |   __ _   _ __   | |_      ___    __ _  | |   ___   _   _  | |   __ _  | |_    ___    _ __ 
+  \___ \   / _ \  | | | |    / _` | | '_ \   / _` |   | '_ \  | |  / _` | | '_ \  | __|    / __|  / _` | | |  / __| | | | | | |  / _` | | __|  / _ \  | '__|
+  ____) | | (_) | | | | |   | (_| | | | | | | (_| |   | |_) | | | | (_| | | | | | | |_    | (__  | (_| | | | | (__  | |_| | | | | (_| | | |_  | (_) | | |   
+ |_____/   \___/  |_| |_|    \__,_| |_| |_|  \__,_|   | .__/  |_|  \__,_| |_| |_|  \__|    \___|  \__,_| |_|  \___|  \__,_| |_|  \__,_|  \__|  \___/  |_|   
+                                                      | |                                                                                                   
+                                                      |_|                                                                                                   """)
+    print("""                                     ------------ 1. Soil Calculator and Purchase  ----------
+                                                  ------------ 2. Plant Calculator and Purchase ----------
+                                                  ------------ 3. Cancel an Order               ----------
+                """)
+    if input() == "1":
+        calcarea()
+    elif input() == "2":
+        plantcalc()
+    elif input() == "3":
+        cancelorder()
